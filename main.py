191c0dff3ce989926cebc2122422b82134903f22
owenlo <owenlo@gmail.com>
2019-02-08 17:12:41 +0000

    Updated mining loop, added save state, plot code removed.
    Replaced the mining loop (a for loop fixed at 100 blocks) to an infinite while loop. 
    
    Added save/load state using Python shelve module. When a SIGINT (e.g. pressing Control + C when running code in terminal) is detected, the code will attempt to save the state of the last mined block using the shelve module. When executing the code again, an attempt will be made to load and resume mining from the last mined block. A file named 'lastBlock.db' is created to enable save/load state.
    
    Plot code has been removed as it does not work well.

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 '''Example blockchain implemented in Python.'''
 import hashlib, block
 import numpy as np
-#import matplotlib.pyplot as plt #Include this import and remove comments on plot(...) function to enable real-time plotting of time taken to mine each block. Note that the code for plotting is quite buggy....
+import shelve
 
 difficulty = 5 #Difficulty of mining operation. Using the example of 5 means that the first five hex values of a hash should be 0 to be considered the correct solution.
 
 def main():
-    #Instantiating a block requires (index, data, previous hash, difficulty)
-    b = block.Block(str(0), 'This is my genesis block data', '0', difficulty)
-    printResult(b)
-    #plot(b.index, b.performance) 
     
-    for i in range(1, 100): #We mine 100 blocks in this example code (including genesis).
-        b = block.Block(str(i),'This is the blocks which follow genesis + i (i is just arbitrary data)' + str(i), b.hash , difficulty)                 
-        printResult(b)
-        #plot(b.index, b.performance) #Remove comment to enable real-time plotting of time taken to mine each block. Note that the code for plotting is quite buggy....
+    b = loadState() #Load the last previous mined block if lastBlock.db file exists
+    
+    try:
+        if b is None:
+            #Instantiating a block requires (index, data, previous hash, difficulty)
+            b = block.Block(str(0), 'This is my genesis block data', '0', difficulty)
+            printResult(b)
 
+        while(True):
+            index = str(int(b.index) + 1)
+            b = block.Block(index,'This is the blocks which follow genesis + i (i is just arbitrary data)' + index, b.hash , difficulty)                 
+            printResult(b)
+
+    except (KeyboardInterrupt, SystemExit):
+        saveState(b)
+
+def saveState(block):
+    '''Try and save the state of the last mined block using the Python shelve module'''
+    with shelve.open('lastBlock') as db:
+        db['lastBlock'] = block
+
+def loadState():
+    '''Try and load the state of last mined block if file exists'''
+    with shelve.open('lastBlock') as db:        
+        block = db.get('lastBlock')
+        if block is None:
+            return None
+        return block
+                     
 def printResult(block):
     #Print the results for each mined block. Nonce is the solution found. Only the final 6 hex values of previous hash is shown for brevity. The genesis block has a previous hash value of 0.
     print(f"Block {block.index} mined. Hash = {block.hash}. Nonce = {block.nonce}. Previous Hash = ...{block.prevhash[-6:]}") 
-    
-
-def plot(index, time):
-    plt.scatter(index, time, linestyle='-', marker='o')
-    plt.pause(0.05)
     
 if __name__ == "__main__":
     main()
